cf416720a14dbebcfb1e7da7a311e443c292815c
Toshimonster <joshuapetercooper@hotmail.co.uk>
2018-03-09 13:50:39 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,8 @@
 module.exports = {
-	TOKEN : ""
+	// The token of the bot. Get yours at https://discordapp.com/developers/applications/me
+	TOKEN : "",
+	// The modules for the bot. They are initialised from left to right. Formatted as "mod1, mod2, mod3"
+	MODULES : "",
+	// 
+	USERNAME : ""
 }
