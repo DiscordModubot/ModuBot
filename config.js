ec80eb8c165c05cfc226faee1c15eb031c9586ae
Toshimonster <joshuapetercooper@hotmail.co.uk>
2018-04-07 19:09:01 +0100

    updates

--- config.js
+++ config.js
@@ -2,7 +2,9 @@ module.exports = {
 	// The token of the bot. Get yours at https://discordapp.com/developers/applications/me
 	TOKEN : "",
 	// The modules for the bot. They are initialised from left to right. Formatted as "mod1, mod2, mod3"
-	MODULES : "",
-	// 
+	MODULES : "help, example",
+	// The prefix for most commands.
+	PREFIX : "$",
+	// Set a username to set to whenever the bot is launched.
 	USERNAME : ""
-}
+};
