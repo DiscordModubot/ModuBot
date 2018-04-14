5aaf69d113ab462e7c1a77270eda753eb534d2f2
Toshimonster <joshuapetercooper@hotmail.co.uk>
2018-04-14 12:32:24 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	// The token of the bot. Get yours at https://discordapp.com/developers/applications/me
-	TOKEN : "MzY4ODU4ODM4Nzg4NjAzOTA0.DbJSZw.XYOJ4hzhUHB0TRzNsWmvYxHDSBI",
+	TOKEN : "",
 	// The modules for the bot. They are initialised from left to right. Formatted as "mod1, mod2, mod3"
 	MODULES : "help, react2role, logger, automod",
 	// The prefix for most commands.
