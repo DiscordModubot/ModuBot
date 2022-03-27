72312c158e98acb2b2a07dd88844d6a17c80d37e
Toshimonster <joshuapetercooper@hotmail.co.uk>
2022-03-27 23:56:50 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	// The token of the bot. Get yours at https://discordapp.com/developers/applications/me
-	TOKEN : "NjkzNTgxMjIwNzEzNTk0OTMx.Xn_KXw.IWAo5PHyXPyB9C40D6X25-qSDb4",
+	TOKEN : "",
 	// The modules for the bot. They are initialised from left to right. Formatted as "mod1, mod2, mod3"
 	MODULES : "help, meowscular, getaroom, newMember, questBoard",
 	// The prefix for most commands.
